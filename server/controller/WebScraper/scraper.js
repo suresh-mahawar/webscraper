d5408053e8e9a8f7eb2a4f8ee00321c41f8ba737
sarvani <annamrajusarvani@cronj.com>
2015-05-04 22:06:59 +0530

    Webscraper with image slider

--- server/controller/WebScraper/scraper.js
+++ server/controller/WebScraper/scraper.js
@@ -11,7 +11,7 @@ exports.getdata = {
     
 
     handler: function(req, reply) {      
-
+        console.log("Hi");
         //loading html of requested url.
         var url=req.payload.productpage;
         request({url:url,headers:{'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.125 Safari/533.4'
